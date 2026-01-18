40119555ce7965bf18260cbfe8fa174980370f7f
Keith Lowry <keith.lowry@shefaschool.org>
2026-01-17 20:11:26 -0500

    Update ChangelogIcon to use SparklesIcon (Heroicon) instead of DocumentTextIcon

--- components/ui/ChangelogIcon.tsx
+++ components/ui/ChangelogIcon.tsx
@@ -30,7 +30,7 @@ export function ChangelogIcon({ portalName }: ChangelogIconProps) {
         title="What's New"
         aria-label="View changelog"
       >
-        <DocumentTextIcon className="w-5 h-5 text-slate-500" />
+        <SparklesIcon className="w-5 h-5 text-slate-500" />
         {unreadCount > 0 && (
           <span className="absolute -top-1 -right-1 w-2 h-2 bg-blue-500 rounded-full border-2 border-white" />
         )}
